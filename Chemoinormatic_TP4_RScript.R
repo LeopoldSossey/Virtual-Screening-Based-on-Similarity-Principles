ef64bd68262e0be3e9fd5d936415e3901d68c0ba
LeopoldSossey <109104647+LeopoldSossey@users.noreply.github.com>
2026-01-08 15:25:25 +0100

    Add files via upload

--- Chemoinormatic_TP4_RScript.R
+++ Chemoinormatic_TP4_RScript.R
@@ -1,11 +1,4 @@
 df_dice_morgan <- read.csv('C:/Users/Sossey/Documents/chemoinformatic/data/dice_morgan_QSAR_result.csv')
-names(df_dice_morgan)
-cor_mat <- cor(df_dice_morgan)
-library(corrplot)
-corrplot(cor_mat,
-         type = 'upper',
-         tl.srt = 45,
-         tl.cex = 0.5)
 model <- lm(Prediction..pIC50. ~ ., data = df_dice_morgan)
 summary(model)
 coefs <- coef(model)
